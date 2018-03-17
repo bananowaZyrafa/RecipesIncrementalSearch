a3045fe8b594bd5556e5167891bf07f08f6304df
bananowaZyrafa <wally93@gmail.com>
2018-03-17 17:35:07 +0100

    Initial Commit

---
+++ RecipiesIncrementalSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecipiesIncrementalSearch
+//
+//  Created by Paweł W. on 17/03/2018.
+//  Copyright © 2018 Bart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
