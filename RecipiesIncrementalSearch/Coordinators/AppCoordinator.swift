551496b2af6411cea3aaaa8921dd1b98078121fb
bananowaZyrafa <wally93@gmail.com>
2018-03-17 17:55:52 +0100

    initial structure, app coordinator implemented

---
+++ RecipiesIncrementalSearch/Coordinators/AppCoordinator.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+import Foundation
+import UIKit
+
+protocol Coordinator: class {
+    func start()
+}
+
+class AppCoordinator: Coordinator {
+    
+    let window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        showRecipies()
+    }
+}
+
+extension AppCoordinator {
+    fileprivate func showRecipies() {
+        let recipiesCoordinator = RecipiesCoordinator(window: window)
+        recipiesCoordinator.start()
+    }
+}
---
+++ RecipiesIncrementalSearch/Details/DetailsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DetailsViewController.swift
+//  RecipiesIncrementalSearch
+//
+//  Created by Paweł W. on 17/03/2018.
+//  Copyright © 2018 Bart. All rights reserved.
+//
+
+import Foundation
